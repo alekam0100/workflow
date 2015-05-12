e90ab25625d563f3a3599f340b8335262c3b8b60
aleks <a.kamenica@kabsi.at>
2015-05-12 18:54:20 +0200

    remove unneccessary imports

--- rest_component/src/main/java/application/controller/GreetingController.java
+++ rest_component/src/main/java/application/controller/GreetingController.java
@@ -3,12 +3,8 @@ package application.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import application.dataaccess.TestRepository;
 import application.domain.Greeting;
