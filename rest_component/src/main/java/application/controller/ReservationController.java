58c64a30a23f8002fec7aa64588f5e457784173a
Ömer Yildirim <oemer.yildirim@outlook.com>
2015-05-18 18:10:50 +0200

    reservationcontroller, getreservationbyid

--- rest_component/src/main/java/application/controller/ReservationController.java
+++ rest_component/src/main/java/application/controller/ReservationController.java
@@ -7,6 +7,7 @@ import org.apache.camel.Exchange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,6 +26,21 @@ public class ReservationController {
 		return reservationService.getAllReservations();
 	}
 
+	public Reservation getReservation(int id){
+		System.out.println("ReservationController.getReservation");
+		System.out.println("id = [" + id + "]");
+		Reservation reservation = new Reservation();
+		reservation.setPkIdReservation(id);
+		return reservation;
+	}
+
+	public List<Reservation> getMyReservations(){
+		List<Reservation> reservations = new ArrayList<>();
+		Reservation reservation = new Reservation();
+		reservation.setPkIdReservation(-42);
+		reservations.add(reservation);
+		return reservations;
+	}
 	public Reservation createReservation(Exchange exchange) {
 		//System.out.println(exchange.getIn().getBody(Reservation.class));
 		return reservationService.createReservation(exchange.getIn().getBody(Reservation.class));
