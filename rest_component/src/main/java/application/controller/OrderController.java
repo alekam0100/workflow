28fdffa9f38c83dd2e21de8e35861ce9b649cac5
aleks <a.kamenica@kabsi.at>
2015-06-07 20:14:20 +0200

    intermediate commit

--- rest_component/src/main/java/application/controller/OrderController.java
+++ rest_component/src/main/java/application/controller/OrderController.java
@@ -1,5 +1,8 @@
 package application.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.camel.Exchange;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +10,12 @@ import org.springframework.stereotype.Component;
 public class OrderController {
 	
 	public void evaluateResult(Exchange exchange) {
-		
+		boolean filterMatched = (boolean)exchange.getProperty(Exchange.FILTER_MATCHED);
+		if(!filterMatched) {
+			exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, 404);
+			Map<String,String> map = new HashMap<String,String>();
+			map.put("error", "Reservation not found");
+			exchange.getOut().setBody(map);
+		}
 	}
 }
