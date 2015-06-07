e52341ec4f82f88712bff5d93fb65f260ff6a1a9
kuhbaar <k.zacky@gmail.com>
2015-06-07 11:50:03 +0200

    forgot to

--- rest_component/src/main/java/application/MyApplicationConfig.java
+++ rest_component/src/main/java/application/MyApplicationConfig.java
@@ -63,7 +63,8 @@ public class MyApplicationConfig {
                 
                 rest().post("/payment").route().process(authProcessor).to("bean-validator:res").to("bean:PaymentController?method=initPayment(*)").choice()
                 .when(header("email").isEqualTo(1)).to("bean:PaymentController?method=sendEmailRegistered(*)")
-                .when(header("email").isNotNull()).validate(header("email").regex("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$"));
+                .when(header("email").isNotNull()).validate(header("email").regex("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$"))
+                .to("bean:PaymentController?method=sendEmail(*)");
                 //Simple email expression. Doesn't allow numbers in the domain name and doesn't allow for top level domains 
                 //that are less than 2 or more than 3 letters (which is fine until they allow more).
             }
