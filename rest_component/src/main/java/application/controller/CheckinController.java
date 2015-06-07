902596e8e55d479ecec1a93c33b338e79520ffce
Vanja Ivljanin <vanjaivljanin@gmail.com>
2015-06-07 20:06:44 +0200

    checkin exception handling

--- rest_component/src/main/java/application/controller/CheckinController.java
+++ rest_component/src/main/java/application/controller/CheckinController.java
@@ -1,39 +1,49 @@
 package application.controller;
 
+import application.exceptions.CheckinException;
 import application.service.CheckinService;
+import application.util.Constants;
 import org.apache.camel.Exchange;
 import org.apache.camel.Header;
 import org.restlet.util.Series;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class CheckinController {
     @Autowired
     private CheckinService checkinService;
 
-    public void checkin(String id, @Header("org.restlet.http.headers") Series<?> headers, Exchange exchange) {
-        if (id == null || id.isEmpty() || headers.getFirstValue("X-timestamp") == null) {
+    public void checkin(String id, @Header("org.restlet.http.headers") Series<?> headers, Exchange exchange) throws CheckinException {
+        if (id == null || id.isEmpty()) {
             exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, "400");
-            exchange.getOut().setBody("No table id or timestamp found");
+            exchange.getOut().setHeader(Constants.ERROR_REASON, "Table not specified");
             return;
         }
 
 
         Integer tableId = Integer.parseInt(id);
-        Timestamp timestamp = Timestamp.valueOf(headers.getFirstValue("X-timestamp"));
-
         try {
-            checkinService.checkIn(tableId, timestamp);
-        } catch (Exception e) {
-            exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, "400");
-            exchange.getOut().setBody(e.getMessage());
-            return;
+            checkinService.checkIn(tableId);
+        } catch (CheckinException e) {
+            exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, 400);
+            exchange.getOut().setHeader(Constants.ERROR_REASON, e.getValue());
+            throw e;
         }
 
         exchange.getOut().setHeader("social", headers.getFirstValue("Social"));
         exchange.getOut().setHeader("message", headers.getFirstValue("Message"));
+    }
+
+    public void handleError(Exchange exchange) {
+        Map<String, String> responseMap = new HashMap<>();
+        String errorMsg = (String)exchange.getOut().getHeader(Constants.ERROR_REASON);
+
+        responseMap.put(Constants.ERROR_REASON, errorMsg);
+        exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, 400);
+        exchange.getOut().setBody(responseMap);
     }
 }
---
+++ rest_component/src/main/java/application/util/Constants.java
@@ -0,0 +1,7 @@
+package application.util;
+
+
+public class Constants {
+
+    public static final String ERROR_REASON = "ERROR_REASON";
+}
